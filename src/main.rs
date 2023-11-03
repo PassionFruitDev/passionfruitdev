71d402eadf8c66a163f9b9f0b093aac4ff1af1f0
Jim Hill <jim.hill.r@gmail.com>
2023-11-02 17:54:32 -0700

    Created publish function

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,7 @@
-use clap::{ValueEnum, Parser, Subcommand};
-
-#[derive(Parser)]
-#[command(author, version, about, long_about = None)]
-struct Cli {
-    #[command(subcommand)]
-    command: Command,
-}
-
-#[derive(Subcommand)]
-enum Command {
-    Install {
-        #[arg(short, long, value_enum, value_enum, default_value_t = Architecture::Aarch64AppleDarwin)]
-        architecture: Architecture,
-        #[arg(long, value_enum, value_enum, default_value_t = OperatingSystem::MacOS)]
-        os: OperatingSystem
-    },
-    New {
-        name: String
-    }
-}
-
-#[derive(ValueEnum, Debug, Clone)]
-#[clap(rename_all = "kebab_case")]
-enum Architecture {
-    Aarch64AppleDarwin
-}
-
-#[derive(ValueEnum, Debug, Clone)]
-enum OperatingSystem {
-    MacOS
-}
+use crate::cli::*;
+pub mod cli;
 
 fn main() {
-    let cli = Cli::parse();
-
-    match &cli.command {
-        Command::Install { architecture, os } => {
-            match (architecture,os) {
-                (Architecture::Aarch64AppleDarwin, OperatingSystem::MacOS) => { println!("Installing Aarch64AppleDarwin on MacOS dependencies"); }
-            }
-        }
-        Command::New { name } => {
-            println!("Creating project: {}",name)
-        }
-    }
+    let cli = cli::Cli::parse();
+    cli::execute(cli)
 }
